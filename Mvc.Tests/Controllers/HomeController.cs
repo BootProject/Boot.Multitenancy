5cd60a557e9e13ea4e9ae2fc489cc5117fcd6cbc
rickardmagnusson <rickard.magnusson.9@gmail.com>
2014-03-09 01:38:00 +0100

    PageViewModel added

---
+++ Mvc.Tests/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Boot.ModelFactory;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //TestModel
+        PageViewModel Model;
+
+        public HomeController()
+        {
+            Model = new PageViewModel();
+        }
+
+        public ActionResult Index()
+        {
+            return View(Model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View(Model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View(Model);
+        }
+    }
+}
---
+++ Mvc.Tests/Models/PageViewModel.cs
@@ -0,0 +1,93 @@
+﻿using Boot.ModelFactory;
+using Boot.Multitenancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+using Boot.Multitenancy.Extensions;
+
+namespace WebApplication1.Models
+{
+
+    //testmodel
+    public class PageViewModel
+    {
+        public ISession Conn { get; set; }
+
+        public PageViewModel()
+        {
+            Conn = SessionFactoryContainer.GetCurrentSession("bootcms.se");
+            
+            InsertPage();
+            CreateSettings();
+
+            Settings = Conn.Get<Settings>(1);
+            Page = Conn.Get<Page>(1);
+        }
+
+        public Page Page
+        {
+            get;
+            set;
+        }
+
+        public Settings Settings
+        {
+            get;
+            set;
+        }
+
+        public List<Page> Pages
+        {
+            get
+            {
+                return Conn.All<Page>();
+            }
+        }
+
+        public void GetEl()
+        {
+            using (var session = SessionFactory.With<Page>().OpenSession())
+            {
+                session.BeginTransaction();
+            }
+        }
+
+        public void CreateSettings()
+        {
+           
+                if (Conn.QueryOver<Settings>().RowCount() == 0)
+                {
+                    var s = new Settings { Id = 1, Title = "Boot Project", FooterText = "(c) All rights reserved Boot Project" };
+                    Conn.Save(s);
+                    Conn.Flush();
+                }
+            
+        }
+
+        //Create first page
+        public void InsertPage()
+        {
+                if (Conn.QueryOver<Page>().RowCount() == 0)
+                {
+                    Page p = new Page()
+                    {
+                        Id = 1,
+                        Action = "Index",
+                        Controller = "Home",
+                        Active = true,
+                        MetaTitle = "Boot Project",
+                        ParentId = 0,
+                        Title = "Start",
+                        Url = ""
+                    };
+                    Conn.Save<Page>(p);
+                    Conn.Flush();
+                }
+              
+        }
+
+
+    }
+}
