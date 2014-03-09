049dbb87e33c44d7d06c29d72800bfc1755a5e60
rickardmagnusson <rickard.magnusson.9@gmail.com>
2014-03-09 17:24:06 +0100

    Added documentation in source.

--- Src/Factory/SessionFactoryData.cs
+++ Src/Factory/SessionFactoryData.cs
@@ -7,23 +7,48 @@ using System;
 
 namespace Boot.Multitenancy
 {
+
+    /// <summary>
+    /// Holder of ISessionFactory and the associated domains.
+    /// </summary>
     internal class SessionFactoryData : IDisposable
     {
+
+
+        /// <summary>
+        /// The key, usually the database name.
+        /// </summary>
         public string Key { get; set; }
+
+
+
+
+        /// <summary>
+        /// List of host header values.
+        /// </summary>
         public List<string> DnsRecords { get; set; }
+
+
+
+
+        /// <summary>
+        /// SessionFactory for this session.
+        /// </summary>
         public ISessionFactory SessionFactory { get; set; }
 
+
+
+
+
+        /// <summary>
+        /// Init a new SessionFactoryData Object.
+        /// </summary>
         public SessionFactoryData() { 
             DnsRecords = new List<string>(); 
         }
         
 
-        public ISessionFactory GetCurrent()
-        {
-            if (DnsRecords.Find(d => d == string.Empty.GetDomain()).IsNotAny())
-                return null;
-            return SessionFactory;
-        }
+
 
         public void Dispose() { }
     }
--- Src/Factory/SessionFactoryHostContainer.cs
+++ Src/Factory/SessionFactoryHostContainer.cs
@@ -8,14 +8,25 @@ using System.Text;
 
 namespace Boot.Multitenancy
 {
+    /// <summary>
+    /// Contains methods to read multiple domains from a single SessionFactory object.
+    /// </summary>
     public class SessionFactoryHostContainer : IDisposable
     {
 
+
+        /// <summary>
+        /// The current SessionFactoryHostContainer.
+        /// </summary>
         public static SessionFactoryHostContainer Current { get; private set; }
+
+
 
 
         private static readonly object Lock = new object();
         internal Dictionary<string, SessionFactoryData> SessionFactories { get; set; }
+
+
 
 
         //Init
@@ -27,6 +38,10 @@ namespace Boot.Multitenancy
         }
 
 
+
+        /// <summary>
+        /// Get the Current ISessionFactory
+        /// </summary>
         public static ISessionFactory CurrentFactory
         {
             get
@@ -48,11 +63,12 @@ namespace Boot.Multitenancy
 
 
         /// <summary>
-        /// Add a SessionFactory named by a Key value
+        /// Add a SessionFactory named by a Key value,
+        /// Associates a ISessionFactory with multiple domains.
         /// </summary>
         /// <param name="key">The key for this connection</param>
         /// <param name="sessionFactory">ISessionFactory to add</param>
-        /// <param name="records">A list of dnsrecords</param>
+        /// <param name="records">A list of dnsrecords(Host header values)</param>
         /// <returns>Current SessionFactoryContainer</returns>
         public SessionFactoryHostContainer Add(string key, ISessionFactory sessionFactory, List<string> records)
         {
